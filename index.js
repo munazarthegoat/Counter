2b392d388caa610f87b649bf2ba16f0f44ee7f6c
munazarthegoat <iaswebads@gmail.com>
2025-03-31 12:21:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const increaseBtn = document.getElementById("increaseBtn");
 const countLabel = document.getElementById("countLabel"); 
 let count = 0;
 
-
 increaseBtn.onclick = function () {
     count++;
     countLabel.textContent = count; 
 };
-
 
 decreaseBtn.onclick = function () {
     if (count > 0) { 
@@ -22,4 +20,5 @@ resetBtn.onclick = function () {
     count = 0;
     countLabel.textContent = count;
 };
+
     
